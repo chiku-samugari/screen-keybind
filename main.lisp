f39d6f9518959279f0625aeacc342f462d79be1b
chiku <samugari@is.s.u-tokyo.ac.jp>
2012-12-03 08:28:10 +0900

    The construction policy of arguments for eval command is changed.
    In the new construction, each argument for eval command is non-quoted
    string and each argument is composed of double-quoted string. The
    arguments are separated by space. Example:
        eval "split"\40"-v" "echo"\40"\"(split -v)\""
    In non-quoted string, \40 works as a space. This space is a component of
    non-quoted string, not a separator of 2 non-quoted strings. If a user
    wants to write a hardstatus string for example, then the user must
    express the contents of escaped string as Lisp string literal.
        hardstatus-string "%%\"%c\""
    This setting shows one % followed by current time wrapped by double
    quotes. The user must not (or, do not have to) escape wrapping double
    quotes. On the other hand, the user must escape the % by prepending
    additional %, by him/herself. In other words, the user must not write
    escape process to make it double-quoted string, but he/she have to write
    escape process to make the contents. What screen-keybind assume is
    really easy thing: the user have to write contents, not a string that
    will be recognized as contents he/she want to express.
    
    How to output one ^ in hardstatus line is a good quiz to understand
    above concept. If we do not use eval and directly use hardstatus
    command, then following is one of the possible answer:
        hardstatus string "\^\^"
    But for binding few commands to a key, we have to use eval. Thus, we
    need a string whose contents is same to "\^\^".
        eval "hardstatus"\40"string"\40"\"\\\^\\\^\""
            or
        eval 'hardstatus string "\"\\\^\\\^\""'
            and so on.
    Both of " and ^ have to be escaped by prepending one \ in double-quoted
    string. The escape for \ is not required in some case, but in this
    example it is required to surely put one \ in front of ^.
    
    Previously, each argument was single-quoted string and each component
    was non-quoted string or double-quoted string. However, single-quoted
    string cannot inlcude a single quote as it's element. Thus I stop to use
    single-quoted string, even though it is really good in some other
    aspects.

--- main.lisp
+++ main.lisp
@@ -72,14 +72,72 @@
         ((stringp strsrc) strsrc)
         (t (error "Malformed string source: ~s" strsrc))))
 
-(defun seval-arg (str)
-  " An argument for eval command of screen must be wrapped
-    with quote."
-  (concat-str "'" str "'"))
+(defun spacing-join (&rest strs)
+  (format nil "~{~a~^ ~}" strs))
 
-(defun sstringarg (str)
-  " A string arguments for screen command must be double quoted."
+(defun join/40 (&rest args)
+  (format nil "~{~a~^\\40~}" args))
+
+(join/40 "split" "-v")
+(princ
+  (apply #'spacing-join
+         (mapcar (papply (apply #'join/40))
+                 '(("split" "-v")
+                   ("focus" "right")
+                   ("other")
+                   ("focus" "left")))))
+
+(defun dqstringify (str)
   (concat-str "\"" str "\""))
+
+(defmacro gen-escape-fn (prepend-char &rest chars)
+  (with-gensyms (c)
+    `(lambda (,c)
+       (if (inq ,c ,@chars)
+         (list ,prepend-char ,c)
+         (list ,c)))))
+
+(defmacro escape (prepend-char str &rest chars)
+  `(coerce (mapcan (gen-escape-fn ,prepend-char ,@chars)
+                   (coerce ,str 'cons))
+           'string))
+
+(macroexpand-1 '(gen-escape-fn #\\ #\" #\# #\^))
+
+(defun make-dqstring (str)
+  " Returns a string whose content is recognized identical to ``str'';{{{
+    in the meaning of screen's double quoted string.
+    Please be aware that ``str'' is a lisp object, string.
+     Actually speaking, this function is a little strange. I mean,
+    it should not exists. For example, if ^ is required to escaped
+    by prepending a \, then it should be  done by the input side. Even
+    if this function exists or not, the input side have to care about very
+    similar point: which character should be escaped? If this function
+    didn't exists, the input side must know which character must be
+    escaped, and construct proper screen-dq-string. This function is
+    only for reducing this annoying part. Thus, I have to do following
+    2 things:
+        1. DO NOT scented the existance of this function!
+        2. Advertise NOTHING must be escaped. Just write the
+           screen string contents, and wrap it by double quotes!
+           The double quotes are for making it to Lisp string,
+           not screen string.
+  ";}}}
+  (dqstringify
+    (escape #\\ str #\" #\# #\^ #\\)))
+
+(join/40 "split" "-v")
+(princ
+  (apply #'spacing-join
+         (mapcar (papply (apply #'join/40))
+                 (maptree #'make-dqstring
+                 '(("split" "-v")
+                   ("focus" "right")
+                   ("other")
+                   ("focus" "left"))))))
+
+(defun make-evalarg (&rest command-components)
+  (apply #'join/40 (mapcar #'make-dqstring command-components)))
 
 (defun state-leave-desc (state)
   (case state
@@ -90,74 +148,104 @@
 (defun state-arrive-desc (state)
   (case state
     (raw "")
-    (screen (seval-arg "command"))
-    (t (seval-arg (concat-str "command -c " (resolve-string state))))))
+    (screen (make-evalarg "command"))
+    (t (make-evalarg "command" "-c" (resolve-string state)))))
 
-(defun spacing-join (&rest strs)
-  (format nil "~{~a~^ ~}" strs))
+(princ (state-arrive-desc 'screen))
+(princ (state-arrive-desc 'snormal))
 
 ;;; desc : string
 ;;; state : string-designator
 ;;; key : string or an integer number or a character. Symbol is NOT allowed.
 ;;;       How to treat octal numbers?
 
-;(defun keybind-desc (key start-state goal-state prior-comdescs post-comdescs)
-;  (string-trim " "
-;               (spacing-join (state-leave-desc start-state)
-;                             (resolve-string key)
-;                             "eval"
-;                             (apply #'spacing-join (mapcar #'seval-arg prior-comdescs))
-;                             (state-arrive-desc goal-state)
-;                             (apply #'spacing-join (mapcar #'seval-arg post-comdescs)))))
+(defun process-evalarg-lst (evalarg-lst)
+  (if (or (null evalarg-lst)
+          (and (null (cdr evalarg-lst))
+               (zerop (length (car evalarg-lst)))))
+    ""
+    (apply #'spacing-join evalarg-lst)))
 
-(defun keybind-desc (key start-state goal-state prior-comdescs post-comdescs)
+(defun keybind-desc (key start-state goal-state prior-evalargs post-evalargs)
   (string-trim " "
                (spacing-join (state-leave-desc start-state)
                              (resolve-string key)
                              "eval"
-                             (if (or (null prior-comdescs)
-                                     (and (null (cdr prior-comdescs))
-                                          (zerop (length (car prior-comdescs)))))
-                               ""
-                               (apply #'spacing-join (mapcar #'seval-arg prior-comdescs)))
-                             (state-arrive-desc goal-state)
-                             (if (or (null post-comdescs)
-                                     (and (null (cdr post-comdescs))
-                                          (zerop (length (car post-comdescs)))))
-                               ""
-                               (apply #'spacing-join (mapcar #'seval-arg post-comdescs))))))
+                             (process-evalarg-lst prior-evalargs)
+                             (state-arrive-desc goal-desc)
+                             (process-evalarg-lst post-evalargs))))
+
+(princ
+  (keybind-desc #\t 'snormal 'snormal (list (make-evalarg "colon"))
+                (list (make-evalarg "echo" "[screen-insert] (colon)"))))
+;;; Another format of above one. These two does same thing.
+(princ
+  (keybind-desc #\t 'snormal 'snormal (list (make-evalarg "colon"))
+                (list (make-evalarg"echo" "\"[screen-insert]\\40(colon)\""))))
+;;; Dec. 3rd 2012, chiku
+;;; As you can aware, one argument for eval command  is a non-quoted screen
+;;; string. In screen-keybind project, it is called EVALARG. In non-quoted
+;;; screen string, \40 works as a separator. Each component of a command is
+;;; double-quoted screen string. In addition, a command sequence is
+;;; space-separated commands. A command sequence is the argument for eval
+;;; command.
+;;;
+;;;  You may feel that the application of MAKE-EVALARG function should be done
+;;; by KEYBIND-DESC function. But it is wrong. In that approach, one EVALARG
+;;; source is space-separated string. In general, we cannot know which space
+;;; should be treated as separaters and which are not. In almost all cases, it
+;;; is possible but bosering.  Here is an example:
+;;;
+;;;     "echo \"[screen-insert] (colon)\""
+;;;
+;;; We have to replace a spce by \40 if it was not inside a double quoted string.
+;;; The important point is, there seems no worth to write that code. This
+;;; KEYBIND-DESC will not be used by a human. On the more higher layer, the
+;;; most comfortable format (at least, most comfortable and natural for me)
+;;; is 2 level list format. Thus, another possible solution is to adopt that
+;;; 2 leve list format on this level:
+;;;
+;;;     `(("echo" ,(make-dqstring "[screen-insert] (colon)")))
+;;;         or
+;;;     (list (list "echo" (make-dqstring "[screen-insert] (colon)")))
+;;;
+;;; I do not know if I should implement it on this layer. I think both of them
+;;; are acceptable.
 
 (keybind-desc #\C 'screen "X"
-              (list "split -v" "focus right" "other" "focus left")
-              (list "echo \"enter [X]\""))
+              (list (make-evalarg "split" "-v") (make-evalarg "focus" "right")
+                    (make-evalarg "other") (make-evalarg "focus" "left"))
+              (list (make-evalarg "echo" "enter [X]")))
 
 (keybind-desc 'c 'screen "X"
-              (list "split -v" "focus right" "other" "focus left")
-              (list "echo \"enter [X]\""))
+              (list (make-evalarg "split" "-v") (make-evalarg "focus" "right")
+                    (make-evalarg "other") (make-evalarg "focus" "left"))
+              (list (make-evalarg "echo" "enter [X]")))
 
 (keybind-desc "^v" 'screen "X"
-              (list "split -v" "focus right" "other" "focus left")
-              (list "echo \"enter [X]\""))
-
-(keybind-desc "v" "X" "X"
-              (list "split -v" "focus right" "other" "focus left")
-              (list "echo \"enter [X]\""))
+              (list (make-evalarg "split" "-v") (make-evalarg "focus" "right")
+                    (make-evalarg "other") (make-evalarg "focus" "left"))
+              (list (make-evalarg "echo" "enter [X]")))
 
 (keybind-desc "^s" "X" "X"
-              (list "split" "focus down" "other" "focus up")
-              (list "echo \"enter [X]\""))
+              (list (make-evalarg "split" "-v") (make-evalarg "focus" "right")
+                    (make-evalarg "other") (make-evalarg "focus" "left"))
+              (list (make-evalarg "echo" "enter [X]")))
 
 (keybind-desc "v" "X" "X"
-              (list "split -v" "focus right" "other" "focus left")
+              (list (make-evalarg "split" "-v") (make-evalarg "focus" "right")
+                    (make-evalarg "other") (make-evalarg "focus" "left"))
               ())
 
 (keybind-desc "v" "X" "X"
-              (list "split -v" "focus right" "other" "focus left")
+              (list (make-evalarg "split" "-v") (make-evalarg "focus" "right")
+                    (make-evalarg "other") (make-evalarg "focus" "left"))
               (list ""))
 
 (defun take-lispcode (x)
   (cadr x))
 
+;;; comitem : string
 (defun comitem-stringify-form (comitem)
   (cond ((atom comitem) (resolve-string comitem))
         ;; This is required for really minor case. If we want to use
@@ -171,19 +259,19 @@
 (comitem-stringify-form '(coerce (list #\l #\e #\f #\t) 'string))
 (comitem-stringify-form '(:lisp (concatenate 'string (list #\c #\l))))
 
-(defun com-stringify-form (com)
+(defun com-evalargify-form (com)
   (if (eq (car com) :lisp)
     (take-lispcode com)
-    `(spacing-join ,@(mapcar #'comitem-stringify-form com))))
+    `(make-evalarg ,@(mapcar #'comitem-stringify-form com))))
 
-(com-stringify-form '(focus left))
-(com-stringify-form '(focus (coerce (list #\l #\e #\f #\t) 'string)))
-(com-stringify-form '(:lisp (concatenate 'string "focus " (concatenate 'string "ri" "ght"))))
+(com-evalargify-form '(focus left))
+(com-evalargify-form '(focus (coerce (list #\l #\e #\f #\t) 'string)))
+(com-evalargify-form '(:lisp (concatenate 'string "focus " (concatenate 'string "ri" "ght"))))
 
 (defun comseq-construct-form (comseq)
   (if (eq (car comseq) :lisp)
     (take-lispcode comseq)
-    `(list ,@(mapcar #'com-stringify-form comseq))))
+    `(list ,@(mapcar #'com-evalargify-form comseq))))
 
 (comseq-construct-form '((split -v) (focus right) (other) (focus left)))
 (comseq-construct-form '((split) (:lisp (string #\c))))
@@ -199,7 +287,7 @@
                          ,(comseq-construct-form prior-coms)
                          ,(comseq-construct-form post-coms))))
 
-;;; COMSEQ-CONSTRUCT-FORM, COM-STRINGIFY-FORM and COMITEM-STRINGIFY-FORM return
+;;; COMSEQ-CONSTRUCT-FORM, COM-EVALARGIFY-FORM and COMITEM-STRINGIFY-FORM return
 ;;; a form that returns one string if evaluated. A function that returns a form
 ;;; is of course quite natural when we are writing macros. What I want to
 ;;; emphasize here is, do not use it in a function definition. It works in
@@ -208,31 +296,56 @@
 (keybind t "^c" raw screen ((focus up)) ())
 (keybind t "^p" window-select window-select
          ((prev))
-         ((echo (sstringarg "[window-select] (prev)"))))
+         ((echo (make-dqstring "[window-select] (prev)"))))
+;;; Both of following two commands outputs [s] as an echo message:
+;;;     1. eval "echo"\40"[s]"
+;;;     2. eval "echo"\40"\"[s]\""
+;;; In 1., [s] is the argument for echo command. The argument for echo command
+;;; in 2. is "[s]".
+;;;     alt-1. echo [s]
+;;;     alt-2. echo "[s]"
 
-(let ((x "focus down")
+;;; The :LISP form allows follwing thing. This example is not preferrable, though.
+(let ((x "'focus down'")
       (y "-v"))
   (keybind nil "j" window-select window-select
            ((:lisp x) (focus up) (split (:lisp y)))
            ()))
 
+(define-symbol-macro escape (format t "~&escape ~a~%" (resolve-string (read))))
+
 (defparameter *hardstatus-string* "")
+
+;;; Dec. 3rd 2012, chiku
+;;; In order to output one ^ in the hardstatus line, a man should provide
+;;; "\"\\^\\^\"" to screen-keybind, and screen-keybind will convert it into
+;;; "\"\\\"\\\\\\^\\\\\\^\\\"\"". That user is responsible for providing the
+;;; first one. This DEFAULT-MESSAGE function has same responsibility. This is
+;;; the reason for why I write the escape process in this function and why
+;;; I do not write such escape process for MESSAGE function.
+(defun default-message (dst primary-command-desc)
+  (make-dqstring
+    (concat-str "%{= }%?%E[" (resolve-string dst) "]"
+                (if (not (zerop (length primary-command-desc)))
+                  (concat-str " ("
+                              (escape #\^ primary-command-desc #\^)
+                              ")")
+                  "")
+                "%:[raw]%?" *hardstatus-string*)))
+
+(defun message (msg &optional (hardstatus-format *hardstatus-string*))
+  (make-dqstring
+    (concat-str msg hardstatus-format)))
 
 (defun set-hardstatus-string (str)
   (progn (setf *hardstatus-string* str)
-         (format t "~&hardstatus string ~a~%" str)))
+         (format t "~&hardstatus string ~a~%"
+                 (default-message 'screen ""))))
 
 (define-symbol-macro hardstatus-string
                      (set-hardstatus-string (read)))
 
-(define-symbol-macro escape (format t "~&escape ~a~%" (resolve-string (read))))
-
-(defun default-message (dst comdesc)
-  (concat-str "%{= }%?%E[" (resolve-string dst) "]"
-              (if (not (zerop (length comdesc)))
-                (concat-str " (" comdesc ")")
-                "")
-              "%:[raw]%?" *hardstatus-string*))
+(set-hardstatus-string "%030=%{B.} %{-}%-w%{=b Mw}%{+u}%{+s}%n %t%{-}%{-}%{-}%+w%{B.} %{-}%=%m/%d %02c")
 
 (defmacro keybind-common (strm key start dst (&rest commands) &optional message)
   " message : any form that is evaluated into a string.
@@ -242,11 +355,13 @@
             ,commands
             (,(cond ((null message)
                      `(hardstatus string
-                                  (:lisp (sstringarg (default-message
-                                                       ',dst
-                                                       ,(com-stringify-form (car commands)))))))
+                                  (:lisp (default-message
+                                           ',dst
+                                           (spacing-join
+                                             ,@(mapcar #'comitem-stringify-form
+                                                       (car commands)))))))
                     ((zerop (length message)) ())
-                    (t `(hardstatus string (:lisp (sstringarg ,message))))))))
+                    (t `(hardstatus string (:lisp (message ,message))))))))
 
 (keybind-common t "j" snormal snormal ((focus down)))
 (keybind-common t "k" screen raw ((focus up)) "focus up")
@@ -257,6 +372,7 @@
 (keybind-common nil "l" snormal snormal ((focus right)) (coerce (coerce "focus right" 'list) 'string))
 (keybind-common nil "l" snormal snormal ((focus right)) "")
 (keybind-common nil "^z" raw screen ())
+(keybind-common t "^" snormal-insert snormal-insert ((stuff ^)))
 
 (defun normalize-cmd-format (cmd)
   (cond ((null cmd) '())
