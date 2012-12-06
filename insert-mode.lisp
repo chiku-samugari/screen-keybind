da66f9a08fbc942f0bc299d80cb7f36946b67bd1
chiku <samugari@is.s.u-tokyo.ac.jp>
2012-12-06 12:21:09 +0900

    [Tiny] Unsed codes and old comments are removed.

--- insert-mode.lisp
+++ insert-mode.lisp
@@ -5,29 +5,11 @@
 ;;; mode during the input.
 (in-package :chiku.genscreenrc)
 
-(remove-if-not #'graphic-char-p (mapcar #'code-char (iota #x80)))
-
-(mapcar #'code-char (iota #x80))
-
-(mapcar (apapply (format t "~s(~:*~c)~10t~7b~10t~:*~o~%" (code-char a0) a0)) (iota #x80))
-
-;;; Okay, I understand the situation.
-(format t "~{bind \\~o eval 'command -c insert'~%~}~%" (iota #x80))
-
-(dolist (octal-desc (mapcar (papply (format nil "\\~o"))
-                            (remove-if (lambda (x) (inq (code-char x) #\ #\))
-                                       (iota #x80))))
-  (keybind-common t octal-desc 'insert 'insert ()))
-
 (defun octal-desc (code)
   (format nil "\\~o" code))
 
 (defun ctrl-code-p (code)
   (< code #b100000))
-
-(ctrl-code-p (char-code #\space))
-(ctrl-code-p (char-code #\Newline))
-(ctrl-code-p (char-code #\!))
 
 (defun use-charname-p (code)
   (or (ctrl-code-p code) (= #o40 code)))
@@ -54,18 +36,3 @@
               (t (aux insert-state (if (eq mode 'transient)
                                      next-state insert-state)
                       "insert")))))))
-
-;;; #\Bel, #\Newline and #\Return : "command", others : "command"\40"-c"\40"screen-insert-layer1"
-;(gen-insert-mode (make-common-istate 'layer '|screen| '|screen| 1))
-;;; all : "command"
-;(gen-insert-mode (make-common-istate 'transient '|screen| '|screen| 1))
-;;; #\Bel : "command"
-;;; others : "command"\40"-c"\40"screen-insert-transient2"
-;(gen-insert-mode (make-common-istate 'transient '|screen| '|screen-insert-transient2| 1))
-;;; #\Bel : "command"\40"-c"\40"snormal"
-;;; #\Newline and #\Return : "command"\40"-c"\40"snormal-insert-layer2"
-;;; others : "command"\40"-c"\40"screen-insert-layer1"
-;(gen-insert-mode (make-common-istate 'layer 'snormal '|snormal-insert-layer2| 1))
-
-;(with-open-file (*standard-output* "insertmode.screen" :direction :output :if-exists :supersede)
-;  (gen-insert-mode (make-common-istate 'layer 'snormal 'snormal 1)))
--- make.lisp
+++ make.lisp
@@ -1,14 +1,12 @@
 (in-package :cl-user)
 
-(require :sb-cltl2)
-
 (load "packages.lisp")
 
 (load "main.lisp")
 
-(load "reader-definitions.lisp")
-
 (load "insert-mode.lisp")
+
+(load "reader-definitions.lisp")
 
 (load "read-rcsource.lisp")
 
@@ -19,5 +17,3 @@
       (chiku.genscreenrc:read-rcsrc (second *posix-argv*)))))
 
 (save-lisp-and-die "gen-screenrc" :toplevel 'main :executable t)
-
-(read-rcsrc "screenrc-src")
